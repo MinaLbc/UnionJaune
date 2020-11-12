c26b0d23b9920378adc5873066fce5a61277a03e
Kybol <kimberley.511@gmail.com>
2020-11-12 17:06:53 +0100

    Camera ++

--- Ink_proto/Assets/Script/CamFollow.cs
+++ Ink_proto/Assets/Script/CamFollow.cs
@@ -9,16 +9,28 @@ public class CamFollow : MonoBehaviour
 
     public float smoothSpeed;
     public Vector3 offset;
+    public float cameraOffset;
 
 
     private void LateUpdate()
     {
-        Vector3 targetPoint = Camera.main.ScreenToWorldPoint(target.position);
+        Vector3 targetPoint = Camera.main.WorldToScreenPoint(target.position);
         float screenWidth = Screen.width;
         float screenHeight = Screen.height;
 
-        if (targetPoint.x <= screenWidth/2 - cameraWidth / 2 || targetPoint.z <= screenHeight/2 - cameraHeight/2)
+        float c_width = screenWidth * cameraWidth;
+        float c_height = screenHeight * cameraHeight;
+        float c_offset = screenHeight * cameraOffset;
+
+        if (targetPoint.x >= screenWidth/2 - c_width / 2 && targetPoint.x <= screenWidth / 2 + c_width / 2 && targetPoint.y >= screenHeight/2 - c_offset - c_height / 2 && targetPoint.y <= screenHeight / 2 - c_offset  + c_height / 2)
         {
+
+        }
+        else
+        {
+            Debug.Log(c_width);
+            Debug.Log(targetPoint);
+            Debug.Log(screenWidth);
             Vector3 desiredPosition = target.position + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
